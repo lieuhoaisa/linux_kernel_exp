e97984c59de37773e8b202f2720d83e56d3ab5d9
vani5hing <lieuhoaisa2614@gmail.com>
2025-08-05 06:45:20 +0700

    small krop

---
+++ pawnyable/lk01_holstein/holstein_v1_stack_overflow/h2_kROP/transfer.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+musl-gcc ./exploit.c -o exploit -static
+mv ./exploit ./root/
+cd root; find . -print0 | cpio -o --null --format=newc > ../rootfs_updated.cpio
+cd ../
+
+qemu-system-x86_64 \
+    -m 64M \
+    -nographic \
+    -kernel bzImage \
+    -append "console=ttyS0 loglevel=3 oops=panic panic=-1 nopti nokaslr" \
+    -no-reboot \
+    -cpu kvm64,+smep,+smap \
+    -gdb tcp::12345 \
+    -smp 1 \
+    -monitor /dev/null \
+    -initrd rootfs_updated.cpio \
+    -net nic,model=virtio \
+    -net user
+
