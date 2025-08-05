5c6d9a59384516e63378351fae9be72218eb213a
vani5hing <lieuhoaisa2614@gmail.com>
2025-08-06 05:49:43 +0700

    fix transfer.sh kpti

---
+++ pawnyable/lk01_holstein/stack_overflow/kaslr/readme.md
@@ -0,0 +1,7 @@
+find kernel base:
+
+```bash
+cat /proc/kallsyms | grep startup_64
+```
+
+with `id = 0`
--- pawnyable/lk01_holstein/stack_overflow/kpti/transfer.sh
+++ pawnyable/lk01_holstein/stack_overflow/kpti/transfer.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 musl-gcc ./exploit.c -o exploit -static
 mv ./exploit ./root/
-cd root; find . -print0 | cpio -o --null --format=newc > ../rootfs_updated.cpio
+cd root; find . -print0 | cpio -o --format=newc --null --owner=root > ../rootfs_updated.cpio
 cd ../
 
 qemu-system-x86_64 \
     -m 64M \
     -nographic \
     -kernel bzImage \
-    -append "console=ttyS0 loglevel=3 oops=panic panic=-1 ptr=on nokaslr" \
+    -append "console=ttyS0 loglevel=3 oops=panic panic=-1 pti=on nokaslr" \
     -no-reboot \
     -cpu kvm64,+smep,+smap \
     -gdb tcp::12345 \
@@ -17,4 +17,3 @@ qemu-system-x86_64 \
     -initrd rootfs_updated.cpio \
     -net nic,model=virtio \
     -net user
-
