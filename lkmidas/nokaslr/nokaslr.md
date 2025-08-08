2d620fb08f95bd63e328576b6e0bcf7b2ef7f466
vani5hing <lieuhoaisa2614@gmail.com>
2025-08-08 20:58:54 +0700

    lkmidas nokaslr

---
+++ lkmidas/nokaslr/nokaslr.md
@@ -0,0 +1,9 @@
+poc for lkmidas kernel series part 2
+
+reading [part 1](https://lkmidas.github.io/posts/20210123-linux-kernel-pwn-part-1/) and [part 2](https://lkmidas.github.io/posts/20210128-linux-kernel-pwn-part-2/) and pawnyable series should be enough to understand this
+
+### kROP: +smep, +smap, kpti=on, no kaslr
+
+no comment
+
+> exploit in the same folder
---
+++ lkmidas/nokaslr/transfer.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+musl-gcc ./exploit.c -o exploit -static
+mv ./exploit ./initramfs
+cd initramfs
+find . -print0 \
+| cpio --null -ov --format=newc \
+| gzip -9 > initramfs.cpio.gz
+mv ./initramfs.cpio.gz ../
+
+cd ..
+
+qemu-system-x86_64 \
+    -m 128M \
+    -cpu kvm64,+smep,+smap \
+    -kernel vmlinuz \
+    -initrd initramfs.cpio.gz \
+    -hdb flag.txt \
+    -snapshot \
+    -nographic \
+    -monitor /dev/null \
+    -no-reboot \
+    -append "console=ttyS0 nokaslr kpti=1 quiet panic=1" \
+    -gdb tcp::1234
